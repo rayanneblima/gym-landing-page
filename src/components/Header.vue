e6c230c6dfbaa882b29ddb785660f681a697e85b
Rayanne Bertolace Lima <rayanne_lima2010@hotmail.com>
2022-07-08 11:13:25 -0300

    add: header component

---
+++ src/components/Header.vue
@@ -0,0 +1,49 @@
+<template>
+  <header>
+    <img
+      class="logo-header"
+      alt="Logo - The Fit Club"
+      src="@/assets/logo.png"
+    />
+
+    <ul class="menu-header">
+      <li>Home</li>
+      <li>Programs</li>
+      <li>Why Us</li>
+      <li>Plans</li>
+      <li>Testimonials</li>
+    </ul>
+  </header>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: 'HeaderComponent'
+})
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+}
+
+.logo-header {
+  height: 3rem;
+  width: 10rem;
+}
+
+.menu-header {
+  color: white;
+  display: flex;
+  gap: 2rem;
+  list-style: none;
+}
+
+.menu-header > li:hover {
+  color: var(--orange);
+  cursor: pointer;
+}
+</style>
